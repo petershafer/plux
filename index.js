09985dd4478ada93772786abc5d2e0350ca343aa
Peter Shafer <peter.shafer@gmail.com>
2017-05-07 20:04:30 -0700

    Adding browserify for module loading support

---
+++ index.js
@@ -0,0 +1,67 @@
+"use strict";
+// Based on description here:
+// https://github.com/facebook/flux/tree/master/examples/flux-concepts
+var plux = (function(){
+    var stores = []; // Contains references to all stores.
+    var noop = function(){};
+    var dispatch = function(action, data){
+        // Iterate through all registered stores to dispatch the action.
+        for(var storeID in stores){
+            var store = stores[storeID];
+            // Handle the action and trigger any mutations.
+            store.handleAction(
+                action, 
+                data, 
+                store.state
+            );
+            store.notify(store.subscriptions);
+        };
+    };
+    var unsubscribe = function(storeName, id){
+        stores[storeName].subscriptions[id] = noop;
+    };
+    var API = {
+        // Register a store with plux to receive actions and manage state.
+        'createStore': function(name, actionHandler, initial){
+            stores[name] = stores[name] || {
+                'state': initial || {},
+                'handleAction': actionHandler,
+                'notify': function(subscriptions){
+                    var store = this;
+                    this.subscriptions.forEach(function(subscription){
+                        subscription(Object.assign({}, store.state));
+                    });
+                },
+                'subscriptions': []
+            };
+        },
+        // Subscribe to listen to any changes that affect a view.
+        'subscribe': function(storeName, subscriber){
+            var subid = stores[storeName].subscriptions.length;
+            stores[storeName].subscriptions.push(subscriber);
+            subscriber(stores[storeName].state);
+            return {
+                "unsubscribe": (function(){
+                    unsubscribe(storeName, subid);
+                }),
+                "id": subid,
+                "store": storeName
+            }
+            ;
+        },
+        // Register an action that's available for views to trigger.
+        'createAction': function(name){
+            // returns callable function that is detached from Plux API.
+            return (function(data){
+                return dispatch(name, data);
+            });
+        },
+        // Allow retrieval of state of specified store
+        'getState': function(name){
+            return Object.assign({}, (stores[name] ? stores[name].state : {}));
+        }
+    }
+    return API;
+})();
+
+module.exports = plux;
